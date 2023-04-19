00d820b11542b10f8e5d8ba18f09f35e680fee63
joaquimbc <joaquimbc@al.insper.edu.br>
2023-04-19 14:17:09 -0300

    Primeiro exercicio - Define posições

---
+++ Batalha-Naval/funcoes_academia.py
@@ -0,0 +1,20 @@
+def define_posicoes(pos_linha, pos_coluna, orientacao, tamanho):
+
+    lista_resultado = [[pos_linha, pos_coluna]]
+
+    if orientacao == 'vertical':
+      i = pos_linha
+      j = 1
+      while j < tamanho:
+          lista_resultado.append([i+1, pos_coluna])
+          i += 1
+          j += 1
+      return lista_resultado
+    
+    i = pos_coluna
+    j = 1
+    while j < tamanho:
+       lista_resultado.append(pos_linha, [i+1])
+       i += 1
+       j += 1
+    return lista_resultado
