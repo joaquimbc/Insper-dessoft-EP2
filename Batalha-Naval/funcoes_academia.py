9dec192996aded84ccfe47f5aa401210751066d2
joaquimbc <joaquimbc@al.insper.edu.br>
2023-04-20 15:22:38 -0300

    Limpeza na função define_posicoes

--- Batalha-Naval/funcoes_academia.py
+++ Batalha-Naval/funcoes_academia.py
@@ -1,20 +1,11 @@
 def define_posicoes(pos_linha, pos_coluna, orientacao, tamanho):
-
-    lista_resultado = [[pos_linha, pos_coluna]]
-
-    if orientacao == 'vertical':
-      i = pos_linha
-      j = 1
-      while j < tamanho:
-          lista_resultado.append([i+1, pos_coluna])
-          i += 1
-          j += 1
-      return lista_resultado
+    lista_resultado = []
     
-    i = pos_coluna
-    j = 1
-    while j < tamanho:
-       lista_resultado.append([pos_linha, i+1])
-       i += 1
-       j += 1
+    if orientacao == 'vertical':
+        for i in range(pos_linha, pos_linha+tamanho):
+            lista_resultado.append([i, pos_coluna])
+    else:
+        for i in range(pos_coluna, pos_coluna+tamanho):
+            lista_resultado.append([pos_linha, i])
+    
     return lista_resultado
